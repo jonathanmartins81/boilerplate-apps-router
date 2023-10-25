6e3d7876cfb6c2b126c9612d12721a65dcfaa768
jonathanmartins81 <jonathanmartins81@gmail.com>
2023-10-25 19:58:05 -0300

    Up README.md

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -9,7 +9,7 @@ describe('<Main />', () => {
 
     // busca o elemento e verifica a existência dele
     expect(
-      screen.getByRole('heading', { name: /react avançado aqua9/i })
+      screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
 
     // gerar snapshot
